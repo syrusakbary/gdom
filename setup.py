228bf780facc4236894951ed71b122828b9c5b42
Syrus Akbary <me@syrusakbary.com>
2016-02-26 11:16:20 -0800

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gdom',
-      version='0.1.0',
+      version='0.1.1',
       download_url='git@github.com:syrusakbary/gdom.git',
       packages=find_packages(),
       author='Syrus Akbary',
